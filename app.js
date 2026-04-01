210f0bba8ca1d9ba9f3f6b149f365cfdc0f33e1a
FernandezVocation <warrenfernandez.it@gmail.com>
2026-04-01 19:27:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -475,7 +475,6 @@ function loadCachedData() {
 async function postToScript(url, body) {
   const resp = await fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
   });
   if (!resp.ok) throw new Error('HTTP ' + resp.status);
